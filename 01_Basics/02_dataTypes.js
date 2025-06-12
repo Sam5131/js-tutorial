33490196f363ab58d0c778e0de2252a4cd769c46
Syamantak Paul <112967459+Sam5131@users.noreply.github.com>
2025-06-12 14:45:46 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";   //treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+// code readibility should be high
+
+let name = "Syamantak"
+let age = 21
+let isLogIn = false
+let state;
+//number => 2^53
+//bigint
+// string => ""
+//boolean => true/false
+//null => stand alone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined)   //undefined
+console.log(typeof null)   //object

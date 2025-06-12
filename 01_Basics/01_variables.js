140dd1ea2b7b790d09dc751a874025c49f0941c2
Syamantak Paul <112967459+Sam5131@users.noreply.github.com>
2025-06-12 14:30:57 +0000

    variable declaration

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 144553 // locked value
+let accountEmail = "Syamanatak@gmail.com"
+var accountPassword = '12345'
+sccountCity = "Jaipur"
+
+let accountState;
+// accountID = 2
+accountEmail = 'hdfc@gmail.com'
+accountPassword = '23213'
+accountCity = "Bengaluru"
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity,accountState])
+
+/* 
+Prefer not to use var because of issue in block scope and functional scope
+*/

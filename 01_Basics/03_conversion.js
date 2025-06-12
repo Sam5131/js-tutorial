7605d155039318bd01b164b81437a8199d9fce81
Syamantak Paul <112967459+Sam5131@users.noreply.github.com>
2025-06-12 14:54:50 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,26 @@
+let score = "Syamantak"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedin = 1
+
+let booleanIsLog = Boolean(isLoggedin)
+console.log(booleanIsLog)
+
+// 1 => true 0 => false
+// "" => false
+// "Sam" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);

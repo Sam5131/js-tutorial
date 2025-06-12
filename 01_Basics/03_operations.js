5294eade29590593eb40109cd74971934f582485
Syamantak Paul <112967459+Sam5131@users.noreply.github.com>
2025-06-12 15:22:17 +0000

    operations on datatypes

---
+++ 01_Basics/03_operations.js
@@ -0,0 +1,33 @@
+let value = 3
+let negValue = -3
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "sam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);   //String first hence all to string
+console.log(1 + 2 + "2");   //number first so all in number
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
